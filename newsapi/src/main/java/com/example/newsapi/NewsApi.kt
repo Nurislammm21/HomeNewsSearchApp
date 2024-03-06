b46d8d66b8f1ceb0aa39f4e68c6b885a46382b6f
Nurislam <gggrrraaa1235@gmail.com>
2024-03-07 00:12:51 +0500

    second commit finished until 27:23 video added interface NewsApi enum class SortBy

--- newsapi/src/main/java/com/example/newsapi/NewsApi.kt
+++ newsapi/src/main/java/com/example/newsapi/NewsApi.kt
@@ -1,10 +1,34 @@
 package com.example.newsapi
 
+import kotlinx.serialization.SerialName
 import retrofit2.http.GET
+import retrofit2.http.Query
+import java.util.Date
 
 interface NewsApi {
 
 
-    @GET()
-    fun everything()
+    @GET("/everything")
+    fun everything(
+        @Query("q") query: String? = null,
+        @Query("from") from: Date? = null,
+        @Query("to") to: Date? = null,
+        @Query("to") languages: String? = null,
+        @Query("sortBy") sortBy: SortBy? = null
+    )
+}
+
+
+// relevancy, popularity, publishedAt.
+enum class SortBy {
+
+    @SerialName("relevancy")
+    RELEVANCY,
+
+    @SerialName("popularity")
+    POPULARITY,
+
+    @SerialName("publishedAt")
+    PUBLISHED_AT
+
 }
